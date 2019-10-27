019a20b9227ec3978b5cf1b375514bcb499e57c2
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-10-26 20:22:34 -0500

    remove app from path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,20 +1,18 @@
-const
-  path = require('path');
+const path = require('path');
 
   module.exports = function(app) {
 
     /*Home Route*/
     app.route('/')
       .get((req, res) => {
-        res.sendFile(path.join(__dirname, './app/public/home.html'));
-
+        res.sendFile(path.join(__dirname, '../public/home.html'));
       })
 
 
     /*Survey Route*/
     app.route('/survey')
       .get((req, res) => {
-        res.sendFile(path.join(__dirname,'./app/public/survey.html'));
+        res.sendFile(path.join(__dirname,'../public/survey.html'));
       });
    
   }
