470a00b7854d45b5de94166260dc2dbad42cdea4
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-10-26 23:33:39 -0500

    added "friends" to friendsList

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,55 @@
 const friendsList =
   [
-    //you have no friends
-  ]
+    {
+      friendName: 'Jeremy',
+      friendpic: 'some oics',
+      scores: [
+        2,
+        5,
+        3,
+        3,
+        2,
+        5,
+        3,
+        4,
+        5,
+        2
+        ]
+    },
+     {
+      friendName: 'Sally',
+      friendpic: 'some oics',
+      scores: [
+        2,
+        4,
+        5,
+        2,
+        2,
+        2,
+        5,
+        3,
+        5,
+        3
+        ]
+    },
+     {
+      friendName: 'Mae',
+      friendpic: 'some oics',
+      scores: [
+        2,
+        2,
+        2,
+        5,
+        3,
+        4,
+        5,
+        2,
+        5,
+        3
+      ]
+    }
+      
+
+  ];
 
 module.exports = friendsList;
