20da3425acb57be2bd1d7ea4b210c0c0dd199c4a
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-10-26 23:36:01 -0500

    add logic for finding friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,15 +4,43 @@ const
   module.exports = function(app) {
 
     /*API Route handles*/
-    app.route('/api/friendsList')
+    app.route('/api/friends')
       .get((req, res) => {
         res.json(friendsList);
       })
 
       .post((req, res) => {
-        friendsList.push(friendsList);
+
+        /*Finding friend */
+        let 
+          searchingFriend = req.body,
+          foundFriendMN = 100,
+          foundFriend;
+
+        let searchingFriendScores = searchingFriend.scores.map(function(score) {
+          return Number.parseInt(score);
+        })
+
+        for(let friend of friendsList) {
+          let matchNumber = 0; //sum of difference
+          let i = 0;
+          
+          for(let score of friend.scores) {
+            
+            let diff = Math.abs(searchingFriendScores[i] - Number.parseInt(score))
+            matchNumber += diff;
+            i++;
+          }
+
+          if(matchNumber < foundFriendMN) {
+            foundfriend = friend[0];
+          } 
+        }
+
+
+
+        friendsList.push(foundFriend);
+        console.log(foundFriend);
         res.json(friendsList);
-      })
-
-
+      });
   }
